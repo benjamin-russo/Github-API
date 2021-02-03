84968e9c58e80c8b7d0669318dee6cf657fd1b24
benjaminrusso-oclock <russobenjamin45@gmail.com>
2021-02-03 12:58:27 +0100

    axios get request & hooks done

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,27 +1,39 @@
 // == Import npm
-import React from 'react';
-// import axios from 'axios';
+import React, { useState } from 'react';
+import axios from 'axios';
 
 // == Import
 // import { Input, Segment, Card, Icon, Image, Form } from 'semantic-ui-react';
 import Cards from 'src/components/Cards';
 import SearchBar from 'src/components/SearchBar';
 import Message from 'src/components/Message';
-import data from 'src/data/repos';
+// import data from 'src/data/repos';
 import './styles.scss';
-
-const handleFormSubmit = (submit) => {
-  console.log("valeur de l'input", submit.target.elements[0].value);
-};
 
 // == Composant
 const App = () => {
+  const [data, setData] = useState();
   console.log(data);
+
+  const handleFormSubmit = (submit) => {
+    console.log("valeur de l'input", submit.target.elements[0].value);
+    const { value } = submit.target.elements[0];
+    axios.get(`https://api.github.com/search/repositories?q=${value}`).then(
+      (response) => (
+        setData(response.data)
+      ),
+    ).catch(
+      (error) => {
+        console.log(error);
+      },
+    );
+  };
+
   return (
     <div className="app">
       <SearchBar handleSubmit={handleFormSubmit} />
-      <Message cardsCount={data.total_count} />
-      <Cards cardsData={data.items} />
+      {data && <Message cardsCount={data.total_count} />}
+      {data && <Cards cardsData={data.items} />}
     </div>
   );
 };
